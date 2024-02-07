bb9067232ff5e38a3fd5baaf46c145aa673a5dd5
Nzy <nidal.chowdhury1@gmail.com>
2024-02-07 12:09:20 +1100

    adv eportfolio

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react"
+import "./style.css"
+import BackgroundLines from "../BackgroundLines"
+import WorkCard from "../WorkCard"
+import ScrambleText from "../ScrambleText"
+import ParaWriting from "../ParaWriting"
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer"
+
+import work1 from "../../assets/Images/work1.png"
+import work2 from "../../assets/Images/work2.png"
+import work3 from "../../assets/Images/work3.png"
+
+export default function Projects() {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+  const [hasAnimated, setHasAnimated] = useState(false)
+
+  const handleComplete = () => {
+    setHasAnimated(true)
+  }
+
+  useEffect(() => {
+    // Start animation when the component is in view
+    if (inView && !hasAnimated) {
+      controls.start("visible")
+    }
+  }, [inView, controls])
+
+  const works = [
+    {
+      client: " ",
+      year: "2023",
+      img: work1,
+      title: "Advanced E-Portfolio",
+      detail: "An engaging ePortfolio developed using HTML, CSS, JavaScript, and React to showcase my skills offering a dynamic representation of my career journey.",
+    },
+    {
+      client: "",
+      year: "2022",
+      img: work2,
+      title: "HULM Studios Project",
+      detail: "The website for Hulm Studios, a digital marketing agency, was meticulously developed using HTML, CSS, and JavaScript. Since then, I have been a part of a number of projects.",
+    },
+    {
+      client: "",
+      year: "2021",
+      img: work3,
+      title: "Twitter Clone",
+      detail: "A Twitter clone web app built using Firebase v9 and React, integrating Firebase v9 Database and Twitter Embeds for content storage and feed fetching",
+    },
+  ]
+
+  const opacityVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
+
+  return (
+    <section ref={ref} className="projects" id="projects">
+      <BackgroundLines />
+      <div className="background--glow"></div>
+
+      <div className="projects--grid">
+        <motion.div initial="hidden" animate={controls} variants={opacityVariant} transition={{ duration: 1, delay: 0.5 }} className="projects--grid--title">
+          <h3 className="theme--text">
+            <ScrambleText shuffle delay={0.5}>
+              04
+            </ScrambleText>{" "}
+            <span className="hash">{"//"}</span>{" "}
+            <ScrambleText shuffle delay={0.5}>
+              Projects
+            </ScrambleText>
+          </h3>
+        </motion.div>
+
+        <div className="projects--grid--content">
+          <div className="projects--grid--content--heading">
+            <h2>
+              <ParaWriting stagger={0.08} text={"My "} sec={"Works"} />
+            </h2>
+          </div>
+          <div className="projects--grid--content--works">
+            {works.map((item, index) => {
+              return (
+                <WorkCard
+                  item={item}
+                  key={index}
+                  // delay={0.1 * index + 1}
+                  // controls={controls}
+                />
+              )
+            })}
+          </div>
+        </div>
+
+        <motion.div initial="hidden" animate={controls} variants={opacityVariant} transition={{ duration: 1, delay: 1 }} onAnimationComplete={() => handleComplete()} className="projects--grid--detail">
+          <p className="theme--detail">
+            <ScrambleText delay={1}>Discover a curated portfolio of projects where each line of code tells a story of problem-solving, creativity, and technical finesse.</ScrambleText>
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  )
+}
